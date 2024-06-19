20ffbd3a83c0381d06b03cf7ab76239a8cd47686
Maria Isabel <76397432+mariaiteixeira@users.noreply.github.com>
2024-06-19 18:40:19 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -13,16 +13,6 @@ var (
 	err error
 )
 
-// func ConectaComBancoDeDados() {
-	
-//stringDeConexao := "host="+os.Getenv("HOST")+ "user="+os.Getenv("USER")+ "password="+os.Getenv("PASSWORD")+ "dbname="+os.Getenv("DBNAME")+ " port="+os.Getenv("PORT")+" sslmode=disable"
-//	DB, err = gorm.Open(postgres.Open(stringDeConexao))
-//	if err != nil {
-//		log.Panic("Erro ao conectar com banco de dados")
-//	}
-
-//	DB.AutoMigrate(&models.Aluno{})
-//}
 func ConectaComBancoDeDados() {
 	stringDeConexao := "host=" + os.Getenv("HOST") + " user=" + os.Getenv("USER") + " password=" + os.Getenv("PASSWORD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("PORT") + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
